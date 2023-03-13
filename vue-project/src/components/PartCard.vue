51b8f57156501c0e8974d5fc22c573268900d69c
AntiProSkills <113395660+gigaquantum@users.noreply.github.com>
2023-03-13 13:15:44 -0400

    commit

---
+++ vue-project/src/components/PartCard.vue
@@ -0,0 +1,3 @@
+<template>
+  <slot>Fallback</slot>
+</template>
--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+// import router from './router'
 
-import './assets/main.css'
+import './assets/style.css'
 
+console.log(app)
 const app = createApp(App)
 
 app.use(router)
