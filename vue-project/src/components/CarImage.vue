ff3d8e1b90068eebe77cc039efa2edaba9e60e19
AntiProSkills <113395660+gigaquantum@users.noreply.github.com>
2023-03-29 18:03:49 -0400

    commit

---
+++ vue-project/src/components/CarImage.vue
@@ -0,0 +1,12 @@
+<script setup>
+import ref from 'vue'
+
+const props = defineProps({
+  imageURL: String,
+  imageAlt: String
+})
+</script>
+
+<template>
+  <img src="{{ imageURL }}" alt="{{ imageAlt }}" />
+</template>
